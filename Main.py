f6ca7b6f723e17190acf93bf983bd92ec258fb45
dirttraxx <dirttraxx@gmail.com>
2020-05-08 06:32:28 -0500

    Prevents user from performing actions without login

--- Main.py
+++ Main.py
@@ -11,6 +11,9 @@ class TrailCounter_MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
+        self.user=""	
+        self.connection_status = ""
+        
         self.addTrailwindow = QtWidgets.QWidget
         self.deleteTrailwindow = QtWidgets.QWidget
         self.addSensorwindow = QtWidgets.QWidget
@@ -49,12 +52,11 @@ class TrailCounter_MainWindow(QtWidgets.QMainWindow):
         else:
             self.SQLcursor = self.cnx.cursor()
             self.connection_status = 'Connected' #to be used to disable other options if not connected
-            self.showTree()
             self.create_login_window()      
 
 
     def showTree(self):
-        query = "SELECT * FROM TRAIL"
+        query = "SELECT * FROM TRAIL WHERE UserName=%s"
         self.SQLcursor.execute(query)
         rows = self.SQLcursor.fetchall()    # get all selected rows, as Barmar mentioned
         
@@ -92,54 +94,69 @@ class TrailCounter_MainWindow(QtWidgets.QMainWindow):
             self.loginWindow.close()
 
     def create_add_trail_window(self):
-        self.addTrailwindow = AddTrail_Window()
-        self.addTrailwindow.show()
-        self.addTrailwindow.ui.Create.clicked.connect(self.create_trail)
-        self.addTrailwindow.ui.Cancel.clicked.connect(self.addTrailwindow.close)
+        if self.user!="":
+            self.addTrailwindow = AddTrail_Window()
+            self.addTrailwindow.show()
+            self.addTrailwindow.ui.Create.clicked.connect(self.create_trail)
+            self.addTrailwindow.ui.Cancel.clicked.connect(self.addTrailwindow.close)
+        else:
+            self.create_login_window()
            
     def create_delete_trail_window(self):
-        username = self.user
-        self.deleteTrailwindow = DeleteTrail_Window()
-        self.deleteTrailwindow.show()
-        query = "SELECT Name FROM TRAIL WHERE Username=%s"
-        values = [username]
-        self.SQLcursor.execute(query,values)
-        rows = self.SQLcursor.fetchall()
-        for r in rows:
-            self.deleteTrailwindow.ui.comboBox.addItem(r[0])
-        self.deleteTrailwindow.ui.Delete.clicked.connect(self.delete_trail)
-        self.deleteTrailwindow.ui.Cancel.clicked.connect(self.deleteTrailwindow.close)
+        if self.user!="":
+            username = self.user
+            self.deleteTrailwindow = DeleteTrail_Window()
+            self.deleteTrailwindow.show()
+            query = "SELECT Name FROM TRAIL WHERE Username=%s"
+            values = [username]
+            self.SQLcursor.execute(query,values)
+            rows = self.SQLcursor.fetchall()
+            for r in rows:
+                self.deleteTrailwindow.ui.comboBox.addItem(r[0])
+            self.deleteTrailwindow.ui.Delete.clicked.connect(self.delete_trail)
+            self.deleteTrailwindow.ui.Cancel.clicked.connect(self.deleteTrailwindow.close)
+        else:
+            self.create_login_window()
         
     def create_add_sensor_window(self):
-        username = self.user
-        self.addSensorwindow = AddSensor_Window()
-        self.addSensorwindow.show()
-        query = "SELECT Name FROM TRAIL WHERE Username=%s"
-        values = [username]
-        self.SQLcursor.execute(query,values)
-        rows = self.SQLcursor.fetchall()
-        for r in rows:
-            self.addSensorwindow.ui.comboBox.addItem(r[0])
-        self.addSensorwindow.ui.Create.clicked.connect(self.create_sensor)
-        self.addSensorwindow.ui.Cancel.clicked.connect(self.addSensorwindow.close)
+        if self.user!="":
+            username = self.user
+            self.addSensorwindow = AddSensor_Window()
+            self.addSensorwindow.show()
+            query = "SELECT Name FROM TRAIL WHERE Username=%s"
+            values = [username]
+            self.SQLcursor.execute(query,values)
+            rows = self.SQLcursor.fetchall()
+            for r in rows:
+                self.addSensorwindow.ui.comboBox.addItem(r[0])
+            self.addSensorwindow.ui.Create.clicked.connect(self.create_sensor)
+            self.addSensorwindow.ui.Cancel.clicked.connect(self.addSensorwindow.close)
+        else:
+            self.create_login_window()
         
     def create_delete_sensor_window(self):
-        username = self.user
-        self.deleteSensorwindow = DeleteSensor_Window()
-        self.deleteSensorwindow.show()
-        query = "SELECT SerialNo FROM SENSOR WHERE UserName=%s"
-        values = [username]
-        self.SQLcursor.execute(query,valuess)
-        rows = self.SQLcursor.fetchall()
-        for r in rows:
-            self.deleteSensorwindow.ui.comboBox.addItem(r[0])
-        self.deleteSensorwindow.ui.Delete.clicked.connect(self.delete_sensor)
-        self.deleteSensorwindow.ui.Cancel.clicked.connect(self.deleteSensorwindow.close)
+        if self.user!="":
+            username = self.user
+            self.deleteSensorwindow = DeleteSensor_Window()
+            self.deleteSensorwindow.show()
+            query = "SELECT SerialNo FROM SENSOR WHERE UserName=%s"
+            values = [username]
+            self.SQLcursor.execute(query,valuess)
+            rows = self.SQLcursor.fetchall()
+            for r in rows:
+                self.deleteSensorwindow.ui.comboBox.addItem(r[0])
+            self.deleteSensorwindow.ui.Delete.clicked.connect(self.delete_sensor)
+            self.deleteSensorwindow.ui.Cancel.clicked.connect(self.deleteSensorwindow.close)
+        else:
+            self.create_login_window()
         
     def create_login_window(self):
-        self.loginWindow = Login_Window()
-        self.loginWindow.show()
-        self.loginWindow.ui.LoginButton.clicked.connect(self.login)
+        if self.connection_status == "Connected":
+            self.loginWindow = Login_Window()
+            self.loginWindow.show()
+            self.loginWindow.ui.LoginButton.clicked.connect(self.login)
+        else:
+            self.create_connect_window()
      
     def create_connect_window(self):
         self.connectWindow = Connect_Window()
@@ -154,7 +171,7 @@ class TrailCounter_MainWindow(QtWidgets.QMainWindow):
         location = self.addTrailwindow.ui.LocationEdit.text()
         #print(trailname)
         query = "INSERT INTO TRAIL (Name, Location, Username) VALUES (%s, %s, %s)"
-        values = (trailname, location, "user1")
+        values = (trailname, location, self.user)
         self.SQLcursor.execute(query, values)
         self.cnx.commit() #Commit the changes
         self.addTrailwindow.close()
@@ -173,7 +190,7 @@ class TrailCounter_MainWindow(QtWidgets.QMainWindow):
         sensorSN = self.addSensorwindow.ui.NameEdit.text()
         trailname = self.addSensorwindow.ui.combBox.currentText()
         query = "INSERT INTO SENSOR (SerialNo, BatteryLife, Position, TrailName, Username) VALUES (%s, %s, %s, %s, %s)"
-        values = (sensorSN, "100", "In the spot", trailname, "user2")
+        values = (sensorSN, "100", "In the spot", trailname, self.user)
         self.SQLcursor.execute(query, values)
         self.cnx.commit()
         self.addSensorwindow.close()
@@ -190,5 +207,6 @@ class TrailCounter_MainWindow(QtWidgets.QMainWindow):
             
         
 
-    #def closeEvent(self, *args, **kwargs):
-        #self.addTrailwindow.close()
+  #  def closeEvent(self, *args, **kwargs):	
+  #      if self.addTrailwindow != NONE: 	
+  #          self.addTrailwindow.close()

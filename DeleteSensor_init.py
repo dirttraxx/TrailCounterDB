fc7342147583d5377a18f5fa110fe7e94cbd53b2
dirttraxx <dirttraxx@gmail.com>
2020-05-08 00:50:48 -0500

    Added other inits, and new main application

---
+++ DeleteSensor_init.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'DeleteSensor.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_NewTrail(object):
+    def setupUi(self, NewTrail):
+        NewTrail.setObjectName("NewTrail")
+        NewTrail.resize(396, 37)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(NewTrail.sizePolicy().hasHeightForWidth())
+        NewTrail.setSizePolicy(sizePolicy)
+        self.gridLayout = QtWidgets.QGridLayout(NewTrail)
+        self.gridLayout.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.gridLayout.setContentsMargins(-1, 6, 6, 6)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.comboBox = QtWidgets.QComboBox(NewTrail)
+        self.comboBox.setObjectName("comboBox")
+        self.horizontalLayout.addWidget(self.comboBox)
+        self.Delete = QtWidgets.QPushButton(NewTrail)
+        self.Delete.setObjectName("Delete")
+        self.horizontalLayout.addWidget(self.Delete)
+        self.Cancel = QtWidgets.QPushButton(NewTrail)
+        self.Cancel.setObjectName("Cancel")
+        self.horizontalLayout.addWidget(self.Cancel)
+        self.horizontalLayout.setStretch(0, 3)
+        self.horizontalLayout.setStretch(1, 1)
+        self.horizontalLayout.setStretch(2, 1)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+
+        self.retranslateUi(NewTrail)
+        QtCore.QMetaObject.connectSlotsByName(NewTrail)
+
+    def retranslateUi(self, NewTrail):
+        _translate = QtCore.QCoreApplication.translate
+        NewTrail.setWindowTitle(_translate("NewTrail", "Delete a Sensor"))
+        self.Delete.setText(_translate("NewTrail", "Delete"))
+        self.Cancel.setText(_translate("NewTrail", "Cancel"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    NewTrail = QtWidgets.QWidget()
+    ui = Ui_NewTrail()
+    ui.setupUi(NewTrail)
+    NewTrail.show()
+    sys.exit(app.exec_())
---
+++ TrailCounterGUI.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from Main import TrailCounter_MainWindow
+
+class main_program(QtCore.QObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.main_window = TrailCounter_MainWindow()
+
+    def close_window(self):
+        self.main_window.close()
+
+app = QtWidgets.QApplication(sys.argv)
+instance = main_program()
+instance.main_window.show()
+instance.main_window.ui.actionAddSensor.triggered.connect(instance.close_window)
+app.exec_()
